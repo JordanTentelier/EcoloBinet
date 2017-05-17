5c2f207411a6b814afd924f3be772a7fa4820c63
Srein62 <jordan.tentelier@hotmail.fr>
2017-05-17 20:47:20 +0200

    Refuse Inter+Rempli without connexion ok

--- Sources/EcoloRemplissage.php
+++ Sources/EcoloRemplissage.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["connecte"]))
+        header('Location:Homepage.php');
+
+?>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
